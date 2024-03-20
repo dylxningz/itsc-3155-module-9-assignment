5f992682c1c5c655f7fccaec928d4e2928f85523
Arthur Khantivong <artbuscus73@gmail.com>
2024-03-20 18:51:30 -0400

    Created tests for task 6

--- tests/e2e/test_delete_movie_page.py
+++ tests/e2e/test_delete_movie_page.py
@@ -1,1 +1,10 @@
-# TODO: Feature 6
+from flask.testing import FlaskClient
+
+def test_delete_movie_page(test_app: FlaskClient):
+
+    response = test_app.delete('/movies/1/delete')
+
+    assert response.status_code == 200
+
+    response_data = response.data.decode('utf-8')
+    assert 'Movie deleted successfully.' in response_data
