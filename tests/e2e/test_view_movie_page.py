bb6dbcee119c14ade16ad7ff03d0a7bad95bf4f4
itsdrelolz <harperandre277@gmail.com>
2024-03-20 23:38:08 -0400

    quick fix to test

--- tests/e2e/test_view_movie_page.py
+++ tests/e2e/test_view_movie_page.py
@@ -1,25 +1,26 @@
-from app import app, get_single_movie
+from app import app
 from src.models.movie import Movie
 from src.repositories.movie_repository import get_movie_repository
 
 
-
-test_movie = Movie(movie_id=1, title='Test Movie', director='Test Director', rating=1)
-
-
-
-movie_repo = get_movie_repository()
+def test_get_movie_by_its_id():
+    # Creates a testing movie object
+    test_movie = Movie(movie_id=1, title='Test Movie', director='Test Director', rating=1)
 
 
 
-movie_repo._db[1] = test_movie       # add test movie to db
+    movie_repo = get_movie_repository()
 
 
-def test_get_single_movie_valid_id():
+
+    movie_repo._db[1] = test_movie       # add test movie to db
+
     with app.test_client() as client:
-        response = client.get('/movies/1')
+        response = client.get('/movies/1')  # test get request to the specific movie id
+
+
+
+
         assert response.status_code == 200
         assert b'Test Movie' in response.data
-
-
-
+        assert b'Test Director' in response.data
