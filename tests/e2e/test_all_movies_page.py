e5790989652ca31d98841d1af5417d142b05a33e
jrobe <jrobe187@uncc.edu>
2024-03-20 21:06:33 -0400

    tests

--- tests/e2e/test_all_movies_page.py
+++ tests/e2e/test_all_movies_page.py
@@ -1,1 +1,15 @@
-# TODO: Feature 1
+from flask.testing import FlaskClient
+import pytest
+
+def test_movies_page(test_app):
+    response = test_app.get('/movies')
+    response_data = response.data.decode('utf-8')
+
+
+    assert '<h1 class="mb-5">All Movies</h1>' in response_data, "Page should have a header 'All Movies'"
+    assert 'See our list of movie ratings below' in response_data, "Page should contain the description text"
+    assert '<table class="table">' in response_data, "Movies should be listed in a table with class 'table'"
+    assert '<th>ID</th>' in response_data, "Table should have an 'ID' column"
+    assert '<th>Title</th>' in response_data, "Table should have a 'Title' column"
+    assert '<th>Director</th>' in response_data, "Table should have a 'Director' column"
+    assert '<th>Rating</th>' in response_data, "Table should have a 'Rating' column"
--- tests/e2e/test_search_movies_page.py
+++ tests/e2e/test_search_movies_page.py
@@ -1,1 +1,8 @@
 # TODO: Feature 3
+from flask.testing import FlaskClient
+
+
+
+def test_search_page(test_app):
+    response = test_app.get('/movies/search')  
+    assert response.status_code == 200
