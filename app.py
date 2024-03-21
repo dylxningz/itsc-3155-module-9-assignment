9e3987ec33b7aaf84e22488f0d2f4ed052bf73ca
itsdrelolz <harperandre277@gmail.com>
2024-03-20 23:16:45 -0400

    added e2e and unit tests

--- app.py
+++ app.py
@@ -51,11 +51,12 @@ def search_movies():
 @app.get('/movies/<int:movie_id>')
 def get_single_movie(movie_id: int):
     # TODO: Feature 4
+    movie = movie_repository.get_movie_by_id(movie_id)
+    if movie is None:
+        return "Movie not found", 404
+
 
     
-  
-
-    movie = movie_repository.get_movie_by_id(movie_id)
     return render_template('get_single_movie.html', movie=movie)
 
 
--- tests/unit/test_get_movie_by_id.py
+++ tests/unit/test_get_movie_by_id.py
@@ -1,27 +1,12 @@
+
 from app import app
-from src.models.movie import Movie
-from src.repositories.movie_repository import get_movie_repository
+from unittest.mock import MagicMock
 
+def test_get_single_movie_not_found():
+    mock_repo = MagicMock()
+    mock_repo.get_movie_by_id.return_value = None  
 
-def test_get_movie_by_its_id():
-    # Creates a testing movie object
-    test_movie = Movie(movie_id=1, title='Test Movie', director='Test Director', rating=1)
-
-
-   
-    movie_repo = get_movie_repository()
-   
-
-
-    movie_repo._db[1] = test_movie       # add test movie to db
-   
-   
     with app.test_client() as client:
-        response = client.get('/movies/1')  # test get request to the specific movie id
-
-
-
-
-        assert response.status_code == 200
-        assert b'Test Movie' in response.data
-        assert b'Test Director' in response.data
+        response = client.get('/movies/2')  
+        assert response.status_code == 404
+        assert b'Movie not found' in response.data 
